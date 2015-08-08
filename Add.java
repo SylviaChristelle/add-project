d012106a8c4560452834c4e5accf7a2ce54ba6d0
SylviaChristelle <sylvia.bascar@a-cti.com>
2015-08-08 10:57:31 +0530

    I have made some more changes on the same program

--- Add.java
+++ Add.java
@@ -2,8 +2,9 @@ import java.io.*;
 class Add
 {
 public static void main(String args[])
-{
+   {
 	int a=3,b=3,c;
 	c=a+b;
-System.out.println("The addition of a and b is "+c);
-}}
+	System.out.println("The addition of a and b is "+c);
+   }
+}
